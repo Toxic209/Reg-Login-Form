2af167b90c4b37016ccba76d11f5caf81491e2c9
Toxic209 <burningsparrow007@gmail.com>
2025-06-28 10:34:16 +0530

    add custom ApiError, ApiResponse, and asyncHandler

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statusCode, data, message = "Success"){
+        this.statusCode = statusCode,
+        this.data = data,
+        this.message = message,
+        this.success = statusCode < 400 //using below 400 for Api Response, above 400 are errors which are handled in ApiError.js
+    }
+}
