fa438e7c45c7a163a063a630adcf04f7c762cef3
Toxic209 <burningsparrow007@gmail.com>
2025-06-28 10:38:06 +0530

    update user database and setup database security

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,24 +1,67 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const userSchema = new mongoose.Schema(
     {
         username: {
             type: String,
             required: true,
             unique: true,
-            lowercase: true,
+            lowercase: true
         },
         email: {
             type: String,
             unique: true,
-            lowercase: true,
+            lowercase: true
         },
         password: {
             type: String,
-            required: true,
+            required: [true, 'Password is required']
         },
+        avatar: {
+            type: String,
+            required: true
+        }, 
+        refreshToken: {
+            type: String
+        }
 
     }, {timestamps: true}
 );
+
+userSchema.pre('save', async function(next){
+    if(!this.isModified("password")) return next()
+    
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+   return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id: this.id,
+        email: this.email,
+        username: this.username
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id: this.id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
 
 
 
