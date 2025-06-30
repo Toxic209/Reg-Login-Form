c0afe56cf8b6905a3785b83fccc38b8e45247b42
Toxic209 <burningsparrow007@gmail.com>
2025-06-30 16:42:34 +0530

    server config for data handling & cloudinary/multer setup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config({
     path : './.env'
 })
 import connectDB from "./db/index.js";
 
 const app = express();
-app.use(express.json())
+app.use(express.json({limit:"20kb"}))
+app.use(express.urlencoded())
+app.use(cookieParser());
 
 export default app;
